3cd288ac743860d8f733a6fa3c32a919f25a09c0
Gloria R <norimaqidoe@hotmail.com>
2026-03-08 00:16:29 -0600

    feat: add email and password to user schema

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const urlRegex = /^https?:\/\/(www\.)?[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=%]+#?$/;
 
@@ -24,6 +25,20 @@ const userSchema = new mongoose.Schema({
       },
       message: "La URL del avatar no es válido",
     },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: "Correo electrónico inválido",
+    },
+  },
+
+  password: {
+    type: String,
+    required: true,
   },
 });
 
